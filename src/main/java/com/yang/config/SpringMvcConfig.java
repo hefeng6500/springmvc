5cddea3919a8a93e6ee51ec2cad3b63fc2ee6f92
hefeng6500 <2443992009@qq.com>
2022-12-30 22:03:58 +0800

    feat: springMVC 入门案例

---
+++ src/main/java/com/yang/config/SpringMvcConfig.java
@@ -0,0 +1,9 @@
+package com.yang.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.yang.controller")
+public class SpringMvcConfig {
+}
