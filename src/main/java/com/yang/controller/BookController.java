132f0d5e859961ec05dc96445de81fb91e632eff
hefeng6500 <2443992009@qq.com>
2022-12-31 17:54:21 +0800

    feat: springMVC Rest 风格 api

--- src/main/java/com/yang/controller/BookController.java
+++ src/main/java/com/yang/controller/BookController.java
@@ -1,12 +1,47 @@
 package com.yang.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
+
+import java.awt.print.Book;
 
 @Controller
 @RequestMapping("/book")
 public class BookController {
+    @RequestMapping(value = "/books",method = RequestMethod.POST)
+    @ResponseBody
+    public String save(@RequestBody Book book){
+        System.out.println("book save..." + book);
+        return "{'module':'book save'}";
+    }
+
+    @RequestMapping(value = "/books/{id}",method = RequestMethod.DELETE)
+    @ResponseBody
+    public String delete(@PathVariable Integer id){
+        System.out.println("book delete..." + id);
+        return "{'module':'book delete'}";
+    }
+
+    @RequestMapping(value = "/books",method = RequestMethod.PUT)
+    @ResponseBody
+    public String update(@RequestBody Book book){
+        System.out.println("book update..." + book);
+        return "{'module':'book update'}";
+    }
+
+    @RequestMapping(value = "/books/{id}",method = RequestMethod.GET)
+    @ResponseBody
+    public String getById(@PathVariable Integer id){
+        System.out.println("book getById..." + id);
+        return "{'module':'book getById'}";
+    }
+
+    @RequestMapping(value = "/books",method = RequestMethod.GET)
+    @ResponseBody
+    public String getAll(){
+        System.out.println("book getAll...");
+        return "{'module':'book getAll'}";
+    }
     @RequestMapping("/save")
     @ResponseBody
     public String save(){
