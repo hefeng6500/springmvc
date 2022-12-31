14b92c039e2b9b0e5954530977778c382005ae16
hefeng6500 <2443992009@qq.com>
2022-12-31 17:58:27 +0800

    feat: springMVC Restful api 简写

--- src/main/java/com/yang/controller/BookController.java
+++ src/main/java/com/yang/controller/BookController.java
@@ -5,47 +5,82 @@ import org.springframework.web.bind.annotation.*;
 
 import java.awt.print.Book;
 
-@Controller
+//@Controller
+@RestController // @Controller + ReponseBody
 @RequestMapping("/book")
 public class BookController {
-    @RequestMapping(value = "/books",method = RequestMethod.POST)
-    @ResponseBody
+    //@RequestMapping(method = RequestMethod.POST)
+    @PostMapping
     public String save(@RequestBody Book book){
         System.out.println("book save..." + book);
         return "{'module':'book save'}";
     }
 
-    @RequestMapping(value = "/books/{id}",method = RequestMethod.DELETE)
-    @ResponseBody
+    //@RequestMapping(value = "/{id}",method = RequestMethod.DELETE)
+    @DeleteMapping("/{id}")
     public String delete(@PathVariable Integer id){
         System.out.println("book delete..." + id);
         return "{'module':'book delete'}";
     }
 
-    @RequestMapping(value = "/books",method = RequestMethod.PUT)
-    @ResponseBody
+    //@RequestMapping(method = RequestMethod.PUT)
+    @PutMapping
     public String update(@RequestBody Book book){
         System.out.println("book update..." + book);
         return "{'module':'book update'}";
     }
 
-    @RequestMapping(value = "/books/{id}",method = RequestMethod.GET)
-    @ResponseBody
+    //@RequestMapping(value = "/{id}",method = RequestMethod.GET)
+    @GetMapping("/{id}")
     public String getById(@PathVariable Integer id){
         System.out.println("book getById..." + id);
         return "{'module':'book getById'}";
     }
 
-    @RequestMapping(value = "/books",method = RequestMethod.GET)
-    @ResponseBody
+    //@RequestMapping(method = RequestMethod.GET)
+    @GetMapping
     public String getAll(){
         System.out.println("book getAll...");
         return "{'module':'book getAll'}";
     }
-    @RequestMapping("/save")
-    @ResponseBody
-    public String save(){
-        System.out.println("book save ...");
-        return "{'module':'book save'}";
-    }
+//    @RequestMapping(value = "/books",method = RequestMethod.POST)
+//    @ResponseBody
+//    public String save(@RequestBody Book book){
+//        System.out.println("book save..." + book);
+//        return "{'module':'book save'}";
+//    }
+//
+//    @RequestMapping(value = "/books/{id}",method = RequestMethod.DELETE)
+//    @ResponseBody
+//    public String delete(@PathVariable Integer id){
+//        System.out.println("book delete..." + id);
+//        return "{'module':'book delete'}";
+//    }
+//
+//    @RequestMapping(value = "/books",method = RequestMethod.PUT)
+//    @ResponseBody
+//    public String update(@RequestBody Book book){
+//        System.out.println("book update..." + book);
+//        return "{'module':'book update'}";
+//    }
+//
+//    @RequestMapping(value = "/books/{id}",method = RequestMethod.GET)
+//    @ResponseBody
+//    public String getById(@PathVariable Integer id){
+//        System.out.println("book getById..." + id);
+//        return "{'module':'book getById'}";
+//    }
+//
+//    @RequestMapping(value = "/books",method = RequestMethod.GET)
+//    @ResponseBody
+//    public String getAll(){
+//        System.out.println("book getAll...");
+//        return "{'module':'book getAll'}";
+//    }
+//    @RequestMapping("/save")
+//    @ResponseBody
+//    public String save(){
+//        System.out.println("book save ...");
+//        return "{'module':'book save'}";
+//    }
 }
